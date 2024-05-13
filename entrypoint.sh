61814a6aeaead5f1586a9e8b67d57c1d3449fda5
Thomas Ardal <thomasardal@gmail.com>
2024-05-13 11:08:26 +0200

    First code

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+set -e # This will cause the script to exit on the first error
+OUTPUT=$(dotnet list package --vulnerable)
+echo "$OUTPUT"
+if echo "$OUTPUT" | grep -q 'no vulnerable packages'; then
+  echo "No vulnerable packages found"
+else
+  if echo "$OUTPUT" | grep -q 'vulnerable'; then
+    echo "Vulnerable packages found"
+    exit 1
+  fi
+fi
